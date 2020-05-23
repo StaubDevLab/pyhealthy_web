8168651b07c68800f3a16fa3012d6ead35042c72
guillaume <guillaume.staub@gmail.com>
2020-05-23 16:39:27 +0200

    Add clean_nutriment method in Utils class in charge of filtering and cleaning the elements from OpenFoodFacts. - Tests OK - Add .coveragerc to configure coverage test

--- pyhealthy/tests_utils.py
+++ pyhealthy/tests_utils.py
@@ -14,9 +14,6 @@ class MockRequestsGet:
         self.result = result
         self.st_code = st_code
 
-    def status_code(self):
-        return self.st_code
-
     def json(self):
         if self.st_code != 200:
             raise requests.exceptions.HTTPError
@@ -34,6 +31,58 @@ results_download = {
         "page": 1,
         "count": 276}
 }
+fake_received_nutriments = {
+    "fat_100g": "10,5",
+    "saturated-fat_100g": "3,5",
+    "sugars_100g": "2,5",
+    "salt_100g": "1,8",
+    "other_nutriment1": "X,Y",
+    "other_nutriment2": "X,Y",
+    "other_nutriment3": "X,Y"
+}
+fake_received_nutriments_prepared = {
+    "fat_prepared_100g": "10,5",
+    "saturated-fat_100g": "3,5",
+    "sugars_prepared_100g": "2,5",
+    "salt_prepared_100g": "1,8",
+    "other_nutriment1": "X,Y",
+    "other_nutriment2": "X,Y",
+    "other_nutriment3": "X,Y"
+}
+fake_received_data = {"products": [{"code": 1,
+                                    "product_name_fr": "name",
+                                    "brands": "brand_name",
+                                    "nutrition_grades": "a_to_e",
+                                    "ingredients_text_fr": "ingredients",
+                                    "url": "url_page",
+                                    "small_image": "url_small_image",
+                                    "large_image": "url_large_image",
+                                    "nutriments": fake_received_nutriments,
+                                    "categories": "oder_categorie",
+                                    "countries_lc": "fr",
+                                    "categories_lc": 'fr',
+                                    "other_param_x": "param_x",
+                                    "other_param_y": "param_y"
+                                    }]
+                      }
+
+fake_result_filtered_nutriments = {'saturated_fat_quantity': "3,5",
+                                   "fat_quantity": "10,5",
+                                   "sugars_quantity": "2,5",
+                                   "salt_quantity": "1,8"
+                                   }
+
+fake_result_filtered_data = [{"id": 1,
+                              "name": "name",
+                              "brand": "brand_name",
+                              "nutrition_grade": "a_to_e",
+                              "ingredients": "ingredients",
+                              "url": "url_page",
+                              "small_image_url": "url_small_image",
+                              "large_image_url": "url_large_image",
+                              "nutriments": fake_result_filtered_nutriments,
+                              "categories": "oder_categorie",
+                              }]
 
 
 def test_download_status_is_ok(monkeypatch, initialize_utils_class):
@@ -47,25 +96,36 @@ def test_download_status_is_ok(monkeypatch, initialize_utils_class):
     assert initialize_utils_class.download() == correct_result
 
 
-def test_download_return_status_not_ok(monkeypatch, initialize_utils_class):
+def test_download_return_status_not_ok(monkeypatch, initialize_utils_class, capsys):
     exception_request_stdout = "There is a problem with the OFF servor or your connection"
 
     def mock_response_not_ok(*args, **kwargs):
         return MockRequestsGet(st_code=400)
 
     monkeypatch.setattr('requests.get', mock_response_not_ok)
-    with pytest.raises(Exception) as e:
-        initialize_utils_class.download()
-        assert str(e) == exception_request_stdout
+
+    initialize_utils_class.download()
+    captured = capsys.readouterr()
+    assert captured.out == exception_request_stdout
 
 
-def test_download_with_keyerror_in_result(monkeypatch, initialize_utils_class):
-    exception_stdout = "There is a problem with the OFF datas return"
+def test_download_with_keyerror_in_result(monkeypatch, initialize_utils_class, capsys):
+    exception_stdout = "There is a problem with the OFF data return"
 
     def mock_response_keyerror(*args, **kwargs):
         return MockRequestsGet(result=results_download["RESULTS_KEYERROR"])
 
     monkeypatch.setattr('requests.get', mock_response_keyerror)
-    with pytest.raises(Exception)as e:
-        initialize_utils_class.download()
-        assert str(e) == exception_stdout
+
+    initialize_utils_class.download()
+    captured = capsys.readouterr()
+    assert captured.out == exception_stdout
+
+
+def test_clean_nutriments_return_good_format(initialize_utils_class):
+    assert initialize_utils_class.clean_nutriments(**fake_received_nutriments) == fake_result_filtered_nutriments
+
+
+def test_clean_nutriments_prepared_return_good_format(initialize_utils_class):
+    assert initialize_utils_class.clean_nutriments(**fake_received_nutriments_prepared) == fake_result_filtered_nutriments
+
--- pyhealthy/utils.py
+++ pyhealthy/utils.py
@@ -11,7 +11,7 @@ class Utils:
             "json": 1,
             "tagtype_0": "categories",
             "tag_contains_0": "contains",
-            "tag_0":category,
+            "tag_0": category,
             "page_size": 250,
             "action": "process"
         }
@@ -19,9 +19,24 @@ class Utils:
     def download(self):
         try:
             products = requests.get(self.api_url_OFF, params=self.api_params_OFF).json()['products']
-        except requests.exceptions:
+            return products
+        except requests.exceptions.HTTPError:
             sys.stdout.write('There is a problem with the OFF servor or your connection')
         except KeyError:
-            print('There is a problem with the OFF data return')
-        return products
+            sys.stdout.write('There is a problem with the OFF data return')
 
+    def clean_nutriments(self, **kwargs):
+        clean_nutriments = {}
+        search_nutriments = ['salt_100g', 'sugars_100g', 'fat_100g', "saturated-fat_100g", "salt_prepared_100g",
+                             "sugars_prepared_100g", "fat_prepared_100g", "saturated-fat_prepared_100g"]
+        for key, value in kwargs.items():
+            if key in search_nutriments:
+                if key == "saturated-fat_100g":
+                    clean_nutriments['saturated_fat_quantity'] = value
+                elif "prepared" in key:
+                    clean_nutriments[key.replace("prepared_100g", "quantity")] = value
+                else:
+                    clean_nutriments[key.replace("100g", "quantity")] = value
+            else:
+                pass
+        return clean_nutriments
