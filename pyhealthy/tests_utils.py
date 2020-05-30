52925dab227e2ac2746bc67127d22e880b7a016b
guillaume <guillaume.staub@gmail.com>
2020-05-30 17:23:33 +0200

    Add clean method in Utils class in charge of filtering and cleaning all elements from OpenFoodFacts. - Tests OK

--- pyhealthy/tests_utils.py
+++ pyhealthy/tests_utils.py
@@ -51,8 +51,37 @@ fake_received_nutriments_prepared = {
 }
 fake_received_categories = "fr:Boissons,Snacks,en:Sugar Snacks,Boissons chaudes,en: Hot Drink"
 
-fake_received_data = {"products": [{"code": 1,
+fake_received_data = [{"code": 1,
                                     "product_name_fr": "name",
+                                    "brands": "brand_name",
+                                    "nutrition_grades": "a_to_e",
+                                    "ingredients_text_fr": "   Ingredient_1, ingredient 2",
+                                    "url": "url_page",
+                                    "selected_images": {
+                                        "front": {
+                                            "display": {
+                                                "fr": "https://static.openfoodfacts.org/images/products/303/371/006"
+                                                      "/5066/front_fr.143.400.jpg",
+                                                "es": "https://static.openfoodfacts.org/images/products/303/371/006"
+                                                      "/5066/front_es.130.400.jpg "
+                                            },
+                                            "small": {
+                                                "fr": "https://static.openfoodfacts.org/images/products/303/371/006"
+                                                      "/5066/front_fr.143.200.jpg",
+                                                "es": "https://static.openfoodfacts.org/images/products/303/371/006"
+                                                      "/5066/front_es.130.200.jpg "
+
+                                            }}},
+                                    "large_image": "url_large_image",
+                                    "nutriments": fake_received_nutriments,
+                                    "categories": fake_received_categories,
+                                    "countries_lc": "fr",
+                                    "categories_lc": 'fr',
+                                    "other_param_x": "param_x",
+                                    "other_param_y": "param_y"
+                                    },
+                                   {"code": 2,
+                                    "product": "name",
                                     "brands": "brand_name",
                                     "nutrition_grades": "a_to_e",
                                     "ingredients_text_fr": "ingredients",
@@ -66,7 +95,6 @@ fake_received_data = {"products": [{"code": 1,
                                     "other_param_x": "param_x",
                                     "other_param_y": "param_y"
                                     }]
-                      }
 
 fake_result_filtered_nutriments = {'saturated_fat_quantity': "3,5",
                                    "fat_quantity": "10,5",
@@ -78,12 +106,14 @@ fake_result_filtered_data = [{"id": 1,
                               "name": "name",
                               "brand": "brand_name",
                               "nutrition_grade": "a_to_e",
-                              "ingredients": "ingredients",
+                              "ingredients": "Ingredient 1, ingredient 2",
                               "url": "url_page",
-                              "small_image_url": "url_small_image",
-                              "large_image_url": "url_large_image",
+                              "small_image_url": "https://static.openfoodfacts.org/images/products/303/371/006"
+                                                 "/5066/front_fr.143.200.jpg",
+                              "large_image_url": "https://static.openfoodfacts.org/images/products/303/371/006"
+                                                 "/5066/front_fr.143.400.jpg",
                               "nutriments": fake_result_filtered_nutriments,
-                              "categories": "oder_categorie",
+                              "categories": fake_result_filtered_categories,
                               }]
 
 
@@ -135,3 +165,11 @@ def test_clean_nutriments_prepared_return_good_format(initialize_utils_class):
 
 def test_clean_categories_return_good_format(initialize_utils_class):
     assert initialize_utils_class.clean_categories(fake_received_categories) == fake_result_filtered_categories
+
+
+def test_clean_select_good_products(initialize_utils_class):
+    assert len(initialize_utils_class.clean(fake_received_data)) == 1
+
+
+def test_clean_return_good_format(initialize_utils_class):
+    assert initialize_utils_class.clean(fake_received_data) == fake_result_filtered_data
--- pyhealthy/utils.py
+++ pyhealthy/utils.py
@@ -43,3 +43,37 @@ class Utils:
 
     def clean_categories(self, cat):
         return [cat.lstrip('fr: ').capitalize() for cat in cat.split(',') if not cat.startswith((' en:', 'en:'))]
+
+    def clean(self, gross_products):
+        clean_products = []
+        need_field = {"code": "id", 'categories': "categories", 'product_name_fr': "name",
+                      'nutrition_grades': "nutrition_grade", 'ingredients_text_fr': "ingredients", 'brands': "brand",
+                      'url': "url", "small_image": "small_image_url",
+                      "large_image": "large_image_url", "nutriments": "nutriments"}
+
+        for product in gross_products:
+            product_item = {}
+            try:
+                if product["product_name_fr"] and product["countries_lc"] == "fr" and product["code"] \
+                        and product["categories_lc"] == "fr":
+                    for key, value in need_field.items():
+
+                        if key == "ingredients_text_fr":
+                            product_item[value] = product[key].replace('_', ' ').lstrip()
+                        elif key == "nutriments":
+                            product_item[value] = self.clean_nutriments(**product[key])
+                        elif key == "large_image":
+                            product_item[value] = product["selected_images"]["front"]["display"]['fr']
+                        elif key == "small_image":
+                            product_item[value] = product["selected_images"]["front"]["small"]['fr']
+                        elif key == "code":
+                            product_item[value] = int(product[key])
+                        elif key == "categories":
+                            product_item[value] = self.clean_categories(product[key])
+                        else:
+                            product_item[value] = product[key]
+                    clean_products.append(product_item)
+
+            except KeyError:
+                continue
+        return clean_products
